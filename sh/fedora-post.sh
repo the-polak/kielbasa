9987c985093592426b3434f98c9eba30906ddedb
Greg <115956307+the-polak@users.noreply.github.com>
2024-11-25 21:41:34 -0500

    Update fedora-post.sh
    updated fedora-post.sh - now runs unattended.

--- sh/fedora-post.sh
+++ sh/fedora-post.sh
@@ -1,110 +1,78 @@
+#!/bin/bash
+
 : '
-     HOWTO: 
+     HOWTO:
              chmod a+x fedora-post.sh
              sudo ./fedora-post.sh
-  
-             Fedora 41 Post-Install 
-  
+
+             Fedora 41 Post-Install
+
              chmod a+x fedora-post.sh
              sudo ./fedora-post.sh
 '
 
-  # Fedora Update
-  echo "Updating OS...."
-  sudo dnf update --refresh
-  sudo dnf upgrade -y
+# Log all output for debugging purposes
+LOGFILE="fedora-post-install.log"
+exec > >(tee -i $LOGFILE) 2>&1
 
-  # Install essential packages
-  echo "Installing essential packages..."
-  sudo dnf install -y git wget curl htop fastfetch
+echo "Starting Fedora post-install script..."
 
-  # Enable RPM Fusion (Free and Non-Free)
-  echo "Enabling RPM Fusion repositories..."
-  sudo dnf install -y \
+# Update and upgrade the system
+echo "Updating OS..."
+sudo dnf update --refresh -y
+sudo dnf upgrade -y
+
+# Install essential packages
+echo "Installing essential packages..."
+sudo dnf install -y git wget curl htop fastfetch
+
+# Enable RPM Fusion repositories (Free and Non-Free)
+echo "Enabling RPM Fusion repositories..."
+sudo dnf install -y \
     https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-  # Install multimedia codecs (optional)
-  echo "Installing multimedia..."
-  dnf group install multimedia
+# Install multimedia codecs (optional)
+echo "Installing multimedia codecs..."
+sudo dnf groupinstall -y multimedia
 
+# Install Development Tools (optional)
+echo "Installing development tools..."
+sudo dnf groupinstall -y "Development Tools" "Development Libraries"
 
+# Set up Fastfetch to run when opening the terminal
+echo "Setting up Fastfetch on terminal startup..."
+echo "fastfetch" >> ~/.bashrc
 
-  # Install Development Tools (optional)
-  echo "Installing development tools..."
-  # sudo dnf groupinstall -y "Development Tools" "Development Libraries" 
-  dnf install @development-tools
-  
-  # Set up Neofetch to run when opening terminal (optional)
-  # echo "Setting up Fastfetch on terminal run..."
-  # echo "fastfetch" >> ~/.bashrc
+# Add Flathub repository
+echo "Ensuring Flathub Repo is added..."
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-  # Automagically Adding Flatpak Repo
-  echo "Ensuring Flathub Repo is added..."
-  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Update Flatpak repositories
+echo "Updating Flatpak repositories..."
+flatpak update -y
 
-  # Update Flatpak repositories
-  echo "Updating Flatpak repositories..."
-  flatpak update -y
+# Install Flatpak applications
+FLATPAK_APPS=(
+    com.nextcloud.desktopclient.nextcloud
+    md.obsidian.Obsidian
+    com.vivaldi.Vivaldi
+    com.visualstudio.code
+    org.videolan.VLC
+    org.onlyoffice.desktopeditors
+    org.localsend.localsend_app
+    net.mullvad.MullvadBrowser
+)
 
-  # Install Nextcloud Desktop - https://github.com/nextcloud/desktop
-  echo "Installing Nextcloud Desktop..."
-  flatpak install -y flathub com.nextcloud.desktopclient.nextcloud
+echo "Installing Flatpak applications..."
+for APP in "${FLATPAK_APPS[@]}"; do
+    echo "Installing $APP..."
+    flatpak install -y flathub "$APP"
+done
 
-  # Install Obsidian - https://github.com/obsidianmd/obsidian-releases
-  echo "Installing Obsidian..."
-  flatpak install -y flathub md.obsidian.Obsidian
+# Clean up the system
+echo "Cleaning up system..."
+sudo dnf autoremove -y
+sudo dnf clean all
 
-  # Install Vivaldi Browser - https://github.com/flathub/com.vivaldi.Vivaldi
-  echo "Installing Vivaldi Browser..."
-  flatpak install -y flathub com.vivaldi.Vivaldi
-
-  # Install Visual Studio Code (VS Code) - https://github.com/microsoft/vscode-docs
-  echo "Installing Visual Studio Code..."
-  flatpak install -y flathub com.visualstudio.code
-
-  # Install VLC Media Player - https://github.com/videolan/vlc
-  echo "Installing VLC Media Player..."
-  flatpak install -y flathub org.videolan.VLC
-
-  # Install OnlyOffice
-  echo "Installing OnlyOffice...."
-  flatpak install -y flathub org.onlyoffice.desktopeditors
-
-  # Install LocalSend - https://github.com/localsend/localsend
-  echo "Installing LocalSend..."
-  flatpak install -y flathub org.localsend.localsend_app
-
-  # Install Mullvad Browser - https://github.com/mullvad/mullvad-browser
-  echo "Installing Mullvad Browser"
-  flatpak install -y flathub net.mullvad.MullvadBrowser   
-
-  # Tailscale - https://tailscale.com/kb/1511/install-fedora-2
-  sudo dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-  && 
-  sudo dnf install tailscale
-  &&
-  &&
-  sudo systemctl enable --now tailscaled
-  &&
-  sudo tailscale up
-
-
-  echo
-
-  # Run KTailctl
-  # echo "Starting KTailctl..."
-  # flatpak run org.fkoehler.KTailctl
-
-  echo
-  echo
-
-  # Clean up system
-  echo "Cleaning up..."
-  sudo dnf autoremove -y
-  sudo dnf clean all -y
-
-  echo
-  echo
-
-  echo "Post-installation script completed!"
+echo "Post-installation script completed! Logs are saved in $LOGFILE."
