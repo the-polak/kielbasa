d284b6cc4fede2b52d6f68760d0e4a91aef25518
Greg <115956307+the-polak@users.noreply.github.com>
2025-06-23 20:40:04 -0700

    Create obsidian-launcher.sh

---
+++ mac-sh/obsidian-launcher.sh
@@ -0,0 +1,5 @@
+# adjust for your preferred app
+# tested on Mac Pro 6,1 w/ Ventura
+# chmod +x ~/obsidian-launcher.sh
+#!/bin/bash
+/Applications/Obsidian.app/Contents/MacOS/Obsidian --disable-gpu --disable-software-rasterizer & 
