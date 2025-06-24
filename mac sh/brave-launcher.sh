c8ae3434eadafe2eab5b24a85c0e2637907d0170
Greg <115956307+the-polak@users.noreply.github.com>
2025-06-23 20:29:04 -0700

    Update and rename to brave-launcher.sh

--- mac sh/brave-launcher.sh
+++ mac sh/brave-launcher.sh
@@ -1,4 +1,5 @@
 # adjust for your preferred app
+# tested on 2013 trashcan Mac Pro w/ Ventura
  
 #!/bin/bash
 /Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser --disable-gpu --disable-software-rasterizer &
